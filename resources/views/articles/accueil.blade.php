682bc3931c91bde337ce19794388062b509cbcbe
Diallo-alpha <alphaloppecity@gmail.com>
2024-05-30 15:47:30 +0000

    redicrection des commentaires sur la même page

--- resources/views/articles/accueil.blade.php
+++ resources/views/articles/accueil.blade.php
@@ -7,7 +7,7 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.84.0">
-    <title>#P7 CRUD_LARA2</title>
+    <title>#P7 CRUD_LARA</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" rel="stylesheet">
